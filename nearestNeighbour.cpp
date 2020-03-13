5d8b5a5e07c3cd605ef5838908384fa7e65ff308
Cody Ray <cody.a.ray@outlook.com>
2020-03-12 18:48:46 -0700

    Cleanup

--- nearestNeighbour.cpp
+++ nearestNeighbour.cpp
@@ -15,7 +15,6 @@ tour nearestNeighbor(std::vector<city> cities, size_t maxTrys = 10) {
 	size_t stepSize = std::max((size_t)1, cities.size()/maxTrys);
 	for(size_t i = 0, j = 0; i < cities.size() && j < maxTrys; i += stepSize) { //Pick maxTrys cities and solve using each as starting city
 		city &startingCity = cities[i];
-		//std::cout << "Starting from #" << startingCity.id;// << std::endl;
 		std::vector<city> thisGraph = {startingCity};
 		unsigned long long thisGraphLength = 0;
 
@@ -25,7 +24,7 @@ tour nearestNeighbor(std::vector<city> cities, size_t maxTrys = 10) {
 		while(unusedCities.size() > 0) { //While we have not gone to all cities
 			city nearest = unusedCities[0];
 			unsigned long long distance = cityDistance(thisGraph[thisGraph.size()-1], nearest);
-			for(size_t i = 1; i < unusedCities.size(); i++) {//for all unusedCities
+			for(size_t i = 1; i < unusedCities.size(); i++) { //for all unusedCities
 				//find the closest city
 				unsigned long long d = cityDistance(thisGraph[thisGraph.size()-1], unusedCities[i]);
 				if(d < distance) {
@@ -49,9 +48,6 @@ tour nearestNeighbor(std::vector<city> cities, size_t maxTrys = 10) {
 		if(thisGraphLength < bestTour.length) {
 			bestTour.cities = thisGraph;
 			bestTour.length = thisGraphLength;
-			//std::cout << "New best: " << thisGraph[0].id << std::endl;
-		} else {
-			//std::cout << "Not best distance: " << thisGraphLength << " vs current " << bestLength << std::endl;
 		}
 	}
 	bestTour.length += cityDistance(bestTour.cities[0], bestTour.cities[bestTour.cities.size()-1]); //Add last connection
